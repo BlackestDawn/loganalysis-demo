eb691a0bc5471be70adccdc6c3871a2f9973515d
Alexander Stauch <blacke4dawn@gmail.com>
2026-04-11 15:36:27 +0200

    updated config to include queue info

--- go-api/internal/config/consts.go
+++ go-api/internal/config/consts.go
@@ -2,3 +2,8 @@ package config
 
 const defaultListenPort = "8080"
 const defaultDbUrl = "postgres://postgres:postgres@localhost:5432/loganalysis?sslmode=disable"
+
+const defaultLogQueueServer = "amqp://guest:guest@localhost:5672/"
+const defaultLogQueueName = "logs"
+const defaultLogQueueKey = "logs"
+const defaultLogQueueExchange = "logs"
